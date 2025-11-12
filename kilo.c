4d34b60fa079548e96a628498a9dc487bffd2642
Adityas-Macbook-Air <120474074+NikSterPrime@users.noreply.github.com>
2025-11-12 12:29:54 +0530

    Second commit

--- kilo.c
+++ kilo.c
@@ -1,5 +1,18 @@
+#include <terminos.h>
+#include <unistd.h>
 
+void enableraw()
+{
+	struct terminos raw;
+
+	tcgetattr(STDIN_FILENO,&raw);
+	raw.c_lflag& = ~(ECHO);
+	tcsetattr(STDIN_FILENO,&raw);
+}
 int main()
 {
-	return 0;	
+	enableraw();
+	char c;
+	while(read(STDIN_FILENO,&c,1)==1 && c!='q');
+	return 0;
 }
