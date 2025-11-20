5d4348238dd1f09ad926cb0cbacd2181bd9002b4
Adityas-Macbook-Air <120474074+NikSterPrime@users.noreply.github.com>
2025-11-20 22:21:07 +0530

    Removed multiple input which would conflict with functionality of a terminal text editor like ctrl-c and ctrl-v, which end process execution.

--- kilo.cpp
+++ kilo.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 #include <termios.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <cctype>
 using namespace std;
 struct termios orig_struct;
 
+void die(const char *s){
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_struct);
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_struct) == -1){
+		die("tcsetattr");
+	}
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_struct);
+    if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_struct) == -1){
+		die("tcsetattr");
+	}
     atexit(disableRawMode);
 
     struct termios raw = orig_struct;
-    raw.c_lflag &= ~(ECHO | ICANON);  // Disable echo + canonical mode
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_iflag &=~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &=~(OPOST);
+    raw.c_cflag |=(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);  // Disable echo + canonical mode
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw)==-1)die("tcsetattr");
 }
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(static_cast<unsigned char>(c))) {
-            cout << (int)c << "\n";
+    while (1) {
+	char c = '\0';
+	if(read(STDIN_FILENO,&c,1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c)) {
+            cout << (int)c << "\r\n";
         } else {
-            cout << (int)c << " (" << c << ")\n";
+            cout << (int)c << " (" << c << ")\r\n";
         }
+	if (c == 'q') break;
     }
 
     return 0;
